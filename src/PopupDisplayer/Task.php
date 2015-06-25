7360a03c3ec62d228b69f1f2f8f0700e43977a26
Flavius12 <info@evolsoft.tk>
2015-06-25 14:57:09 +0200

    Update Task.php

--- src/PopupDisplayer/Task.php
+++ src/PopupDisplayer/Task.php
@@ -8,20 +8,19 @@ use pocketmine\scheduler\PluginTask;
 class Task extends PluginTask {
 	private $message;
 	
- public function __construct(Main $plugin, $message, $duration){
+ public function __construct(Main $plugin, $duration){
  	parent::__construct($plugin);
         $this->plugin = $plugin;
-        $this->message = $message;
         $this->duration = $duration;
         $this->current = 0;
     }
     
     public function onRun($tick){
     	$this->plugin = $this->getOwner();
-    	$message = $this->cfg->get("message");
+    	$message = $this->plugin->cfg->get("message");
     	if($this->current <= $this->duration){
     		foreach($this->plugin->getServer()->getOnlinePlayers() as $players){
-    			$players->sendPopup($this->plugin->translateColors("&", $this->message));
+    			$players->sendPopup($this->plugin->translateColors("&", $message));
     		}
     	}else{
     		$this->plugin->getServer()->getScheduler()->cancelTask($this->getTaskId());
