b478137070ab1704f1e8b8da3f0b9747901100d8
Heromine <jonnimperato@yahoo.it>
2015-06-25 15:25:44 +0200

    Update Main.php

--- src/PopupDisplayer/Main.php
+++ src/PopupDisplayer/Main.php
@@ -26,10 +26,10 @@ class Main extends PluginBase implements Listener{
 		$this->saveDefaultConfig(); //salva la configurazione di default del config.yml
 		$this->cfg = $this->getConfig(); //prende le informazioni dal config.yml
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getServer()->getScheduler()->scheduleRepeatingTask(new Task($this, $this->cfg->get("duration")), 10);
 	}	
 	
 	public function onPlayerJoin(PlayerJoinEvent  $event){
+		$this->getServer()->getScheduler()->scheduleRepeatingTask(new Task($this, $this->cfg->get("duration")), 10);
 		$type = $this->cfg->get("type");
 		$message = $this->cfg->get("message");
 		if($type == "tip"){
