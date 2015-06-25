b1d2bb2679ed8ea42ccfb4dd5a312069db8b1c69
Flavius12 <info@evolsoft.tk>
2015-06-25 14:56:40 +0200

    Update Main.php

--- src/PopupDisplayer/Main.php
+++ src/PopupDisplayer/Main.php
@@ -13,40 +13,34 @@ use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener{
 
-public $cfg;
+	public $cfg;
 	
 	public function onLoad(){
 		$this->getLogger()->info("Plugin Enabled");  //getLogger() mostra il messaggio dopo info nella console di PM
-		}
+	}
 	
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-			@mkdir($this->getDataFolder()); //crea la cartella dove sara il config.yml
-				$this->saveDefaultConfig(); //salva la configurazione di default del config.yml
-					$this->cfg = $this->getConfig(); //prende le informazioni dal config.yml
-							$this->getServer()->getScheduler()->scheduleRepeatingTask(new Task($this, $message, $cfg["duration"]), 10);
+		@mkdir($this->getDataFolder()); //crea la cartella dove sara il config.yml
+		$this->saveDefaultConfig(); //salva la configurazione di default del config.yml
+		$this->cfg = $this->getConfig(); //prende le informazioni dal config.yml
+		$this->getServer()->getScheduler()->scheduleRepeatingTask(new Task($this, $message, $cfg["duration"]), 10);
 	}	
 	
 	public function onPlayerJoin(PlayerJoinEvent  $event){
-			$type = $this->cfg->get("type");
-				if($type === "tip"){
-					$message = $this->cfg->get("message");
-						$this->cfg->set("message",$message);
-							$this->cfg->save();
-								$event->getPlayer()->sendTip($message);
-							
+		$type = $this->cfg->get("type");
+		$message = $this->cfg->get("message");
+		if($type == "tip"){
+			$event->getPlayer()->sendTip($message);
 		}elseif($type === "popup"){
-				$message = $this->cfg->get("message");
-					$this->cfg->set("message",$message);
-						$this->cfg->save();
-							$event->getPlayer()->sendPopup($message);
-								}
-							}
+			$event->getPlayer()->sendPopup($message);
+		}
+	}
 
 public function onDisable(){
-			$this->saveDefaultConfig();
-				$this->getLogger()->info("Plugin Disabled");
-		}
+		$this->saveDefaultConfig();
+		$this->getLogger()->info("Plugin Disabled");
+	}
 }
 
 ?>
